056fc81d05762776de882346c9dbedf17a248764
Jaap Versteegh <j.r.versteegh@gmail.com>
2022-10-09 23:50:38 +0200

    Add build task on windows

--- tasks.py
+++ tasks.py
@@ -1,10 +1,12 @@
+import platform
+
 from invoke import task
 
 
 @task
 def format(ctx):
     """Run black and isort"""
-    for cmd in ("black .", "isort .", "pandoc -s -o README.rst README.md"):
+    for cmd in ("black .", "isort ."):
         ctx.run(cmd, echo=True)
 
 
@@ -23,22 +25,39 @@ def test(ctx):
 
 
 @task
-def build(ctx):
-    """Build"""
+def build_doc(ctx):
     for cmd in (
-        "poetry env use 3.8",
-        "poetry update",
-        "poetry install",
-        "poetry run poetry build",
-        "poetry env use 3.9",
-        "poetry update",
-        "poetry install",
-        "poetry run poetry build",
-        "poetry env use 3.10",
-        "poetry update",
-        "poetry install",
-        "poetry run poetry build",
         "sphinx-apidoc -P -f -o docs/source src/pygeofun",
         "sphinx-build -b html docs dist/docs",
     ):
+        ctx.run(cmd, echo=True)
+
+
+@task
+def build(ctx):
+    """Build"""
+    cmds = []
+
+    if platform.platform().startswith("Win"):
+        for pyver in ("3.8.10", "3.9.13", "3.10.7"):
+            cmds += [
+                f"pyenv install {pyver}",
+                f"pyenv local {pyver}",
+                "pyenv local",
+                "pyenv exec python --version",
+                "pyenv exec poetry env use python",
+                "pyenv exec poetry update",
+                "pyenv exec poetry install",
+                "pyenv exec poetry run poetry build",
+            ]
+    else:
+        for pyver in ("3.8", "3.9", "3.10"):
+            cmds += [
+                f"poetry env use {pyver}",
+                "poetry update",
+                "poetry install",
+                "poetry run poetry build",
+            ]
+
+    for cmd in cmds:
         ctx.run(cmd, echo=True)
