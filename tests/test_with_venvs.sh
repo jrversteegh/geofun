795e581e566197f04f349f0f53c64c5700db915c
Jaap Versteegh <j.r.versteegh@orca-st.com>
2023-09-01 00:32:10 +0200

    Only allow binary download in test

--- tests/test_with_venvs.sh
+++ tests/test_with_venvs.sh
@@ -5,7 +5,7 @@ cd /tmp
 for ver in 3.8 3.9 3.10 3.11; do
   python$ver -m venv venv
   . venv/bin/activate
-  pip install pygeofun
+  pip install --only-binary pygeofun pygeofun || echo "########### Failed to get pygeofun"
   python --version
   echo "import geofun; print(f'GEOFUN VERSION: {geofun.get_version()}')" | python
   deactivate
